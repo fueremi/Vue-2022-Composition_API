3edfb290b7ce07a2ddd493e2cbb65d16aff9526d
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2022-03-09 13:42:02 +0700

    initiate project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from "vue";
+
+const counter = ref(0);
+
+const increaseCounter = () => counter.value++;
+const decreaseCounter = () => counter.value--;
+</script>
+
+<template>
+  <main>
+    <h1>Home</h1>
+
+    <h3>Counter</h3>
+    <button class="button" @click="decreaseCounter">-</button>
+    <span>{{ counter }}</span>
+    <button class="button" @click="increaseCounter">+</button>
+  </main>
+</template>
+
+<style scoped>
+h1 {
+  margin-bottom: 0.7rem;
+}
+h3 {
+  margin-bottom: 10px;
+}
+.button {
+  border: 1px solid #ebebeb;
+  background: #fff;
+  border-radius: 5px;
+  padding: 8px 16px;
+}
+span {
+  margin: 0 16px;
+  font-size: 1.3rem;
+  color: hsla(160, 100%, 37%, 1);
+}
+</style>
